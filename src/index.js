cb26deb6b8036df64d4accde1993c6c6414158af
DanielPinto <danielsp.pinto@gmail.com>
2021-03-29 21:08:54 -0300

    instalação do expo-file-system e expo-document-picker e foi feito as implementações de pegar o arquivo csv e converter para JSON e listar na tela.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,103 @@
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Button, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Button, View, FlatList,Text } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
-
+import * as FileSystem from 'expo-file-system';
+import { set } from 'lodash';
 
 
 export default function Home() {
+
+
+  const [document, setDocument] = useState([]);
+
+
+  useEffect(()=>{
+    if(document!=[]){  
+
+      
+      console.log(document);
+    }
+      
+  }),(document);
+
+async function getUri(){
+
+  let uri = "uri null";
+  await DocumentPicker.getDocumentAsync().then((res)=>{
+    uri = res.uri;
+  });
+  return uri;
+};
+
+
+
+async function readDocument(uri){
+
+  let file = null;
+  await FileSystem.readAsStringAsync(uri).then((res)=>{
+    file = res;
+  });
+  return file;
+}
+
+async function getDocument(){
+
+    await getUri().then(
+      async function(uri){
+        await readDocument(uri).then((file)=>{
+          let jsonDoc = csvToJson(file);
+          setDocument(jsonDoc);
+        });
+      }
+    )
+ 
+};
+
+console.log("fora do getDocument");
+
+
+function csvToJson(csv){
+ 
+  var lines=csv.split("\n");
+
+  var result = [];
+
+  var headers=lines[0].replace(/\s/g, '').split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+	  var obj = {};
+	  var currentline=lines[i].split(",");
+
+	  for(var j=0;j<headers.length;j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+
+	  result.push(obj);
+
+  }
   
+  //return result; //JavaScript object
+  return JSON.parse(JSON.stringify(result)); //JSON
 
-    
-const [state, setState] = useState(0);
+}
 
-    useEffect(
-        ()=>{
-    
-            alert(state);
-        }
-        ),(state);
-    
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      
+      <FlatList
+      data={document}
+      renderItem={({item})=><Text>{item.NomeUnidade}</Text>}
+      />
       <Button 
       title="CSV"
-      onPress={()=>setState(state+1)}/>
-    </View>
+      onPress={()=>getDocument()}/>
+
+    
+    </SafeAreaView>
   );
 
 
